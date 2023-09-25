e3a2c78c978b1870a35c1ced7c735913ac59ad52
Guilherme Miranda <62404655+FuLLeNN@users.noreply.github.com>
2023-09-25 14:26:09 +0200

    Fix Home Page

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
+      //set NODE_OPTIONS=--openssl-legacy-provider
     <>
       <motion.div
         className={HomeStyles.home}
@@ -19,27 +20,34 @@ export default function Home() {
             transition: {ease: "easeInOut"},
           },
         }}>
-        <div className={HomeStyles.introtext}>
-          <motion.h1
-            animate={{scale: 1, opacity: 1, y: "0"}}
-            transition={{
-              delay: 0.25,
-              duration: 0.75,
-              type: "spring",
-              stiffness: 190,
-            }}
-            initial={{scale: 0, opacity: 0, y: "-100vh"}}>
-            Welcome.
-          </motion.h1>
-          <motion.p
-            animate={{scale: 1, opacity: 1}}
-            transition={{delay: 0.5, duration: 0.75}}
-            initial={{scale: 0, opacity: 0}}>
-            Hi, this is my personal portfolio that was made in Next.js and here
-            you can see more about me. <br />
-            Below you can see my resume.
-          </motion.p>
-        </div>
+          <div className={HomeStyles.imageContainer}>
+              <img src="/foto3.jpeg" alt="Portfolio Picture" className={HomeStyles.image}/>
+              <h3>Guilherme Miranda</h3>
+              <span className={HomeStyles.imageContent}>Software Developer</span>
+          </div>
+          <div className={HomeStyles.introtext}>
+              <motion.h1
+                  animate={{ scale: 1, opacity: 1, y: "0" }}
+                  transition={{
+                      delay: 0.25,
+                      duration: 0.75,
+                      type: "spring",
+                      stiffness: 190,
+                  }}
+                  initial={{ scale: 0, opacity: 0, y: "-100vh" }}
+              >
+                  Welcome.
+              </motion.h1>
+              <motion.p
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{ delay: 0.5, duration: 0.75 }}
+                  initial={{ scale: 0, opacity: 0 }}
+              >
+                  Hi, this is my personal portfolio that was made in Next.js and here
+                  you can see more about me. <br />
+                  Below you can see my resume.
+              </motion.p>
+          </div>
         <Link href="/CVGuilhermeMiranda.pdf" passHref>
           <a target="_blank">
             <motion.button
